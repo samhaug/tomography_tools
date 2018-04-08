d8f17bbcee764cc55e7721d7ef7730809f60b829
Samuel Haugland <samhaug@umich.edu>
2018-04-08 18:53:44 -0400

    commit

---
+++ ttime_plot.py
@@ -0,0 +1,26 @@
+#!/home/samhaug/anaconda2/bin/python
+
+'''
+==============================================================================
+
+File Name : ttime_plot.py
+Purpose : ---
+Creation Date : 08-04-2018
+Last Modified : Sun 08 Apr 2018 06:18:23 PM EDT
+Created By : Samuel M. Haugland
+
+==============================================================================
+'''
+
+import numpy as np
+from matplotlib import pyplot as plt
+from subprocess import call
+from os import listdir
+import h5py
+import obspy
+import seispy
+
+def main():
+
+
+main()
